d4abc3f706f071d8afd7a8ac7766f7bec804e453
W. Trevor King <wking@drexel.edu>
2012-01-24 13:55:37 -0500

    Run update_copyright.py.

--- pycomedi/utility.py
+++ pycomedi/utility.py
@@ -1,17 +1,19 @@
-# Copyright (C) 2010  W. Trevor King
+# Copyright (C) 2011-2012 W. Trevor King <wking@drexel.edu>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This file is part of pycomedi.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# pycomedi is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 2 of the License, or (at your
+# option) any later version.
+#
+# pycomedi is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with pycomedi.  If not, see <http://www.gnu.org/licenses/>.
 
 "Useful utility functions and classes"
 
--- update_copyright.py
+++ update_copyright.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 #
-# Copyright (C) 2009-2012 W. Trevor King <wking@drexel.edu>
+# Copyright (C) 2012 W. Trevor King <wking@drexel.edu>
 #
-# This file is part of Bugs Everywhere.
+# This file is part of pycomedi.
 #
-# Bugs Everywhere is free software; you can redistribute it and/or modify it
+# pycomedi is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation, either version 2 of the License, or (at your
 # option) any later version.
 #
-# Bugs Everywhere is distributed in the hope that it will be useful, but
+# pycomedi is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Bugs Everywhere.  If not, see <http://www.gnu.org/licenses/>.
+# along with pycomedi.  If not, see <http://www.gnu.org/licenses/>.
 
 """Automatically update copyright boilerplate.
 
