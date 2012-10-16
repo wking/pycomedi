334cb1ebedd202e366c836ed291ca0f64f6374b3
W. Trevor King <wking@tremily.us>
2012-10-16 15:45:28 -0400

    test.sh: use Python-3-compatible print().

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ nosetests --with-doctest pycomedi
 ls pycomedi | grep '.pyx$'| while read file; do
   mod="${file/.pyx/}"
   echo "$mod"
-  python -c "import doctest, sys; import pycomedi.$mod as m; r = doctest.testmod(m); print r; sys.exit(r.failed)"
+  python -c "import doctest, sys; import pycomedi.$mod as m; r = doctest.testmod(m); print(r); sys.exit(r.failed)"
 done
 nosetests --with-doctest --doctest-extension=.txt doc
 nosetests --with-doctest doc/demo/*.py
