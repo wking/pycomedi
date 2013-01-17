wking/pycomedi

commit ac38a7987800c3982ac363af5b7780885c88d3a1

calibration: support to-physical-only softcal boards.
This is a hack to fix Éric Piel's issue:

On Thu, Oct 18, 2012 at 12:10:49PM +0200, Éric Piel wrote:
> Unfortunately, with my device there seems to still be a problem with
> comedi_get_softcal_converter(), which returns -1 with direction
> from_physical. I get the same problem when using the SWIG wrapper, so
> it's probably not an error in pycomedi. So when I run cmd.py I get this
> error, but it works anyway as the _to_phys converter works:
> ./cmd.py -f /dev/comedi0 -s 0 -c 0 -a diff -N 1 -p
> Exception pycomedi.PyComediError:
> PyComediError('comedi_get_softcal_converter: Resource temporarily
> unavailable (-1)',) in
> 'pycomedi.channel.AnalogChannel.get_softcal_converter' ignored
> 0.220157

I feel like we should make an effort to invert the from_physical
polynomial, but as a stop-gap measure, we can just raise the original
exception and let the user decide what to do about it.

pycomedi/channel.pyx
@@ -420,23 +420,28 @@ cdef class AnalogChannel (Channel):
         cdef _comedilib_h.comedi_polynomial_t to_physical, from_physical
         cdef _CalibratedConverter ret
         flags = self.subdevice.get_flags()
+        from_physical_error = None
         if flags.soft_calibrated:
             if calibration is None:
                 calibration = self.subdevice.device.parse_calibration()
             to_physical = self.get_softcal_converter(
                 _constant.CONVERSION_DIRECTION.to_physical,
                 calibration)
-            from_physical = self.get_softcal_converter(
-                _constant.CONVERSION_DIRECTION.from_physical,
-                calibration)
+            try:
+                from_physical = self.get_softcal_converter(
+                    _constant.CONVERSION_DIRECTION.from_physical,
+                    calibration)
+            except _PyComediError as e:
+                from_physical_error = e
         else:
             to_physical = self.get_hardcal_converter(
                 _constant.CONVERSION_DIRECTION.to_physical)
             from_physical = self.get_hardcal_converter(
                 _constant.CONVERSION_DIRECTION.from_physical)
-        ret = _CalibratedConverter()
+        ret = _CalibratedConverter(from_physical_error=from_physical_error)
         ret._to_physical = to_physical
-        ret._from_physical = from_physical
+        if from_physical_error is not None:
+            ret._from_physical = from_physical
         return ret
 
     def get_converter(self, calibration=None):

pycomedi/calibration.pxd
@@ -21,6 +21,7 @@ cimport _comedilib_h
 
 cdef class CalibratedConverter (object):
     cdef _comedilib_h.comedi_polynomial_t _to_physical, _from_physical
+    cdef object _from_physical_error
 
     cdef _str_poly(self, _comedilib_h.comedi_polynomial_t polynomial)
     cpdef to_physical(self, data)

pycomedi/calibration.pyx
@@ -161,11 +161,32 @@ cdef class CalibratedConverter (object):
     1.0
     >>> c.get_to_physical_coefficients()
     array([ 1.,  2.,  3.])
+
+    For some soft-calibrated boards, there is no from_physical
+    conversion polynomial.
+
+    >>> c = CalibratedConverter(
+    ...     from_physical_error=Exception('no conversion polynomial'))
+    >>> c.from_physical(1.0)
+    Traceback (most recent call last):
+      ...
+    Exception: no conversion polynomial
+
+    However, even with the error, you can extract dummy coefficients.
+
+    >>> c.get_from_physical_expansion_origin()
+    0.0
+    >>> c.get_from_physical_coefficients()
+    array([ 0.])
     """
+    def __cinit__(self):
+        self._from_physical_error = None
+
     def __init__(self, to_physical_coefficients=None,
                  to_physical_expansion_origin=0,
                  from_physical_coefficients=None,
-                 from_physical_expansion_origin=0):
+                 from_physical_expansion_origin=0,
+                 from_physical_error=None):
         if to_physical_coefficients:
             _setup_comedi_polynomial_t(
                 &self._to_physical, to_physical_coefficients,
@@ -174,6 +195,7 @@ cdef class CalibratedConverter (object):
             _setup_comedi_polynomial_t(
                 &self._from_physical, from_physical_coefficients,
                  from_physical_expansion_origin)
+        self._from_physical_error = from_physical_error
 
     cdef _str_poly(self, _comedilib_h.comedi_polynomial_t polynomial):
         return '{coefficients:%s origin:%s}' % (
@@ -194,6 +216,8 @@ cdef class CalibratedConverter (object):
                         _constant.CONVERSION_DIRECTION.to_physical)
 
     cpdef from_physical(self, data):
+        if self._from_physical_error is not None:
+            raise self._from_physical_error
         return _convert(&self._from_physical, data,
                         _constant.CONVERSION_DIRECTION.from_physical)
 

commit 27562b04a5480b8dc703703711f179e4d139ab15

channel: Fix to-physical setup in AnalogChannel._get_converter
This fixes a bug introduced by

  commit ac38a7987800c3982ac363af5b7780885c88d3a1
  Author: W. Trevor King <wking@tremily.us>
  Date:   Fri Oct 19 09:53:22 2012 -0400

    calibration: support to-physical-only softcal boards.

where the converter's _from_physical was only initialized if there was
an error trying to determine its value.  Obviously, we should only be
initializing it if there *wasn't* an error.

pycomedi/channel.pyx
@@ -441,7 +441,7 @@ cdef class AnalogChannel (Channel):
                 _constant.CONVERSION_DIRECTION.from_physical)
         ret = _CalibratedConverter(from_physical_error=from_physical_error)
         ret._to_physical = to_physical
-        if from_physical_error is not None:
+        if from_physical_error is None:
             ret._from_physical = from_physical
         return ret
 